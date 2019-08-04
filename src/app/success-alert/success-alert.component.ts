85a20e570caf03dbaaf74c1f2f3493eb46aa4d19
Vanita Bhoyar <vanita.bhoyar282@gmail.com>
2019-08-03 23:33:56 -0700

    added styles logic

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  styleUrls: ['./success-alert.component.css']
+  //styleUrls: ['./success-alert.component.css']
+  styles:[`
+  p{
+    color: green;
+  }
+  `]
 })
 export class SuccessAlertComponent implements OnInit {
 
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -4,7 +4,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'warning-alert-root',
   templateUrl: './warning-alert.component.html',
-  styleUrls: ['./warning-alert.component.css']
+  //styleUrls: ['./warning-alert.component.css']
+  styles:[`
+  p{
+    color: red;
+  }
+  `]
 })
 export class WarningAlertComponent implements OnInit{
     constructor(){}
