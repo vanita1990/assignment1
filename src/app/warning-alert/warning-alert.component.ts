09c8e27592c3998ac8e1aa48d63ef7a2473f5e29
Vanita Bhoyar <vanita.bhoyar282@gmail.com>
2019-08-03 22:49:14 -0700

    initial commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'warning-alert-root',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent i
